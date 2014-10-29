4e4ea495fc1fb345bdfd4adcbf472f89f9a31953
Gray Gilmore <gray@pixelunion.net>
2014-10-29 15:09:39 -0700

    Switch to four spaces per indent

--- tasks/shopify_sass.js
+++ tasks/shopify_sass.js
@@ -13,38 +13,38 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('shopify_sass', 'Concatenate your Sass files defined by the @@import order.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    grunt.registerMultiTask('shopify_sass', 'Concatenate your Sass files defined by the @@import order.', function() {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            punctuation: '.',
+            separator: ', '
+        });
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function(f) {
+            // Concat specified files.
+            var src = f.src.filter(function(filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function(filepath) {
+                // Read file source.
+                return grunt.file.read(filepath);
+            }).join(grunt.util.normalizelf(options.separator));
+
+            // Handle options.
+            src += options.punctuation;
+
+            // Write the destination file.
+            grunt.file.write(f.dest, src);
+
+            // Print a success message.
+            grunt.log.writeln('File "' + f.dest + '" created.');
+        });
     });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
 
 };
