b0fff2ef70f277b515feca20f91c206921ec8374
Gray Gilmore <graygilmore@gmail.com>
2014-10-30 10:33:47 -0700

    Update test files, we're passing!

--- test/shopify_sass_test.js
+++ test/shopify_sass_test.js
@@ -23,26 +23,26 @@ var grunt = require('grunt');
 */
 
 exports.shopify_sass = {
-  setUp: function(done) {
-    // setup here if necessary
-    done();
-  },
-  default_options: function(test) {
-    test.expect(1);
+    setUp: function(done) {
+        // setup here if necessary
+        done();
+    },
+    default: function(test) {
+        test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+        var actual = grunt.file.read('tmp/default.scss.liquid');
+        var expected = grunt.file.read('test/expected/default');
+        test.equal(actual, expected, 'Content from test.scss should be in test.scss.liquid.');
 
-    test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
+        test.done();
+    },
+    multiple_src: function(test) {
+      test.expect(1);
 
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+      var actual = grunt.file.read('tmp/multiple.scss.liquid');
+      var expected = grunt.file.read('test/expected/multiple_src');
+      test.equal(actual, expected, 'Content from both test.scss and test-2.scss should be in test.scss.liquid.');
 
-    test.done();
-  },
+      test.done();
+    }
 };
