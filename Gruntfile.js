59fb7cb9078e14db3c9072f15bd862403892a897
Gray Gilmore <gray@pixelunion.net>
2014-10-29 17:06:57 -0700

    Change test files to match plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,20 +30,9 @@ module.exports = function(grunt) {
 
         // Configuration to be run (and then tested).
         shopify_sass: {
-            default_options: {
-                options: {
-                },
+            default: {
                 files: {
-                    'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-                }
-            },
-            custom_options: {
-                options: {
-                    separator: ': ',
-                    punctuation: ' !!!'
-                },
-                files: {
-                    'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+                    "tmp/test.scss.liquid": "test/fixtures/test.scss"
                 }
             }
         },
